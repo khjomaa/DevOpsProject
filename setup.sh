1a1ec5532e0c7e96824addfaab0b6585d59a6f52
Khalil Jomaa <khjomaa@gmail.com>
2020-04-01 19:14:35 +0300

    updated jenkins values and setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ minikube start --cpus 2 --memory 6144 --vm-driver virtualbox
 
 echo "" && echo "### Install Jenkins ###"
 kubectl apply -f jenkins/namespace.yaml
-kubectl apply -f jenkins/clusterrolebinding.yaml
 helm upgrade --install --wait jenkins stable/jenkins -f jenkins/my-values.yaml -n jenkins
+kubectl apply -f jenkins/clusterrolebinding.yaml
 kubectl -n jenkins port-forward svc/jenkins 8080:8080 &
